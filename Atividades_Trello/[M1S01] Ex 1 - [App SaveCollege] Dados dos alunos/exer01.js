95f622f8ed39d02b921aba0577515dc1db01f4cd
AssisFreitas <francisco_silva21@estudante.sesisenai.org.br>
2023-02-11 12:17:34 -0300

    Estrutura de Pastas

---
+++ Atividades_Trello/[M1S01] Ex 1 - [App SaveCollege] Dados dos alunos/exer01.js
@@ -0,0 +1,41 @@
+var alunos = [
+    {
+        name: "Maria",
+        height: 1.71,
+        age: 25,
+        sex: "F",
+        room: "Trindade"
+    },
+    {
+        name: "José",
+        height: 1.85,
+        age: 43,
+        sex: "M",
+        room: "Trindade"
+    },
+    {
+        name: "João",
+        height: 1.55,
+        age: 22,
+        sex: "M",
+        room: "Trindade"
+    },
+    {
+        name: "Ricardo",
+        height: 1.86,
+        age: 40,
+        sex: "M",
+        room: "Trindade"
+    },
+    {
+        name: "Joana",
+        height: 1.65,
+        age: 40,
+        sex: "F",
+        room: "Trindade"
+    }
+];
+
+for (let i = 0; i < alunos.length; i++) {
+    console.log(`Name: ${alunos[i].name}, Height: ${alunos[i].height}, Age: ${alunos[i].age}, Sex: ${alunos[i].sex}, Room: ${alunos[i].room}`)
+}
